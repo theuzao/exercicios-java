8a7ba4cb2b6c1770248888fb39bac4b1b1e93ea5
Matheus Guilherme <104092352+theuzao@users.noreply.github.com>
2025-02-16 23:09:17 -0300

    Update matematica15.java

--- basico/matematica15.java
+++ basico/matematica15.java
@@ -7,25 +7,12 @@ public class Exercicio1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Digite as coordenadas do primeiro ponto:");
+		System.out.print("Digite o valor do raio da esfera: ");
+		double raio = sc.nextDouble();
 
-		System.out.print("Coordenada x: ");
-		double x1 = sc.nextDouble();
+		double volume = (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
 
-		System.out.print("Coordenada y: ");
-		double y1 = sc.nextDouble();
-
-		System.out.println("\nDigite as coordenadas do segundo ponto:");
-
-		System.out.print("Coordenada x: ");
-		double x2 = sc.nextDouble();
-
-		System.out.print("Coordenada y: ");
-		double y2 = sc.nextDouble();
-
-		double distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-
-		System.out.println("\nA distância entre os pontos é: " + distancia);
+		System.out.println("O volume da esfera é: " + volume);
 
 		sc.close();
 	}
